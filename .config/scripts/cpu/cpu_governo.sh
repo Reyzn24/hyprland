2d9fdd0de184fb9f284f924c7270b7261733a3d7
Reyzn24 <netindaspaty.bvrc@gmail.com>
2025-07-16 01:21:48 -0400

    Limpando scripts

--- .config/scripts/cpu/cpu_governo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Lê o modo atual do escalonador de frequência da CPU (governador) do primeiro núcleo (cpu0)
-GOVERNOR=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-
-# Exibe no terminal o modo atual da CPU (por exemplo: performance, powersave, etc.)
-echo "CPU: $GOVERNOR"
-
