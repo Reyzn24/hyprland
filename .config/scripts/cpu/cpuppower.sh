a2e02bc9d83684d64dbb7d830197db7468d99288
Reyzn24 <netindaspaty.bvrc@gmail.com>
2025-07-16 01:29:46 -0400

    Implement power governor switcher script with status check

---
+++ .config/scripts/cpu/cpuppower.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+# Verifica se cpupower está instalado
+if ! command -v cpupower >/dev/null || ! command -v kitty >/dev/null; then
+  echo "cpupower é kitty não encontrados."
+  read -p "Deseja instalar cpupower e kitty agora? (s/n): " resposta
+  if [[ "$resposta" =~ ^[Ss]$ ]]; then
+    echo "Instalando cpupower e kitty..."
+    sudo pacman -S --noconfirm cpupower kitty
+    if [ $? -ne 0 ]; then
+      echo "Falha na instalação do cpupower e kitty . Abortando."
+      exit 1
+    fi
+  else
+    echo "cpupower é kitty necessário para o script funcionar. Abortando."
+    exit 1
+  fi
+fi
+
+HYPRCONF="$HOME/.config/hypr/hyprland.conf"
+BACKUP="$HYPRCONF.bak"
+
+# Se quiser remover os prints depois de configurado:
+VERBOSE=false
+
+if ! grep -q "class:gov-cpu" "$HYPRCONF"; then
+  $VERBOSE && echo "Adicionando regras em hyprland.conf para gov-cpu..."
+
+  if [ ! -f "$BACKUP" ]; then
+    cp "$HYPRCONF" "$BACKUP"
+    $VERBOSE && echo "Backup criado em $BACKUP"
+  fi
+
+  cat <<EOF >> "$HYPRCONF"
+
+# Regras para terminal flutuante de controle de CPU
+windowrulev2 = float, class:gov-cpu
+windowrulev2 = center, class:gov-cpu
+windowrulev2 = size 600 200, class:gov-cpu
+EOF
+
+  echo "Hyprland.conf atualizado"
+  echo "Regras adicionadas. Reinicie ou recarregue o Hyprland."
+fi
+
+# Obs: Entradas em ingles pois acho legal assim. 
+
+clear
+echo "Select CPU governor:"
+echo "1) power-save"
+echo "2) balanced"
+echo "3) performance"
+echo "4) Check current governor"
+echo -n "#? "
+read -r escolha
+
+case "$escolha" in
+  1)
+    sudo cpupower frequency-set -g powersave
+    echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo > /dev/null
+    notify-send "Mode: Power-Save" "Governor: powersave"
+    ;;
+  2)
+    sudo cpupower frequency-set -g schedutil
+    echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo > /dev/null
+    notify-send "Mode: Balanced" "Governor: schedutil"
+    ;;
+  3)
+    sudo cpupower frequency-set -g performance
+    echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo > /dev/null
+    notify-send "Modo: Performance" "Governor: performance | Turbo Boost: enable"
+    ;;
+  4)
+    GOVERNOR=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+    notify-send "CPU Governor:" "$GOVERNOR"
+    ;;
+  *)
+    echo "Opção inválida!"
+    exit 1
+    ;;
+esac
